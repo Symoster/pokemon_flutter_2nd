160b83a0c46327fd17df2013b4cb5c476d6b1b4a
Symoster <mcoc7synyster@gmail.com>
2022-06-07 21:09:52 +0900

    2022/6/7　エラー解決の為

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,199 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_flutter_2nd/poke_detail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Home());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ThemeMode mode = ThemeMode.system;
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Pokemon Flutter 2nd',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: mode,
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-
-  final String title;
+class TopPage extends StatefulWidget {
+  const TopPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TopPageState createState() => _TopPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TopPageState extends State<TopPage> {
+  int currentbnb = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: currentbnb == 0 ? const PokeList() : const Settings(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => {
+          setState(
+            () => currentbnb = index,
+          ),
+        },
+        currentIndex: currentbnb,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
+class PokeList extends StatelessWidget {
+  const PokeList({Key? key}) : super(key: key);
 
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+      itemCount: 898,
+      itemBuilder: (context, index) => PokeListItem(index: index),
+    );
+  }
+}
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  ThemeMode _themeMode = ThemeMode.system;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          leading: const Icon(Icons.lightbulb),
+          title: const Text('Dark/Light Mode'),
+          trailing: Text((_themeMode == ThemeMode.system)
+              ? 'System'
+              : (_themeMode == ThemeMode.dark ? 'Dark' : 'Light')),
+          onTap: () async {
+            var ret = await Navigator.of(context).push<ThemeMode>(
+              MaterialPageRoute(
+                builder: (context) => ThemeModeSelectionPage(
+                    mode:
+                        _themeMode), //modeの部分はinitを入力しろとかいてあったが、initだとエラーが出た。modeでエラーが解消したのでmodeを使用した。
+              ),
+            );
+            setState(() => _themeMode = ret!);
+          },
+        )
+      ],
+    );
+  }
+}
+
+class ThemeModeSelectionPage extends StatefulWidget {
+  const ThemeModeSelectionPage({
+    Key? key,
+    required this.mode,
+  }) : super(key: key);
+  final ThemeMode mode;
+
+  @override
+  _ThemeModeSelectionPageState createState() => _ThemeModeSelectionPageState();
+}
+
+class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
+  late ThemeMode _current;
+  @override
+  void initState() {
+    super.initState();
+    _current = widget.mode;
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body: Center(
+        body: SafeArea(
+            child: Column(
+      children: [
+        ListTile(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop<ThemeMode>(context, _current),
+          ),
+        ),
+        RadioListTile<ThemeMode>(
+          value: ThemeMode.system,
+          groupValue: _current,
+          title: const Text('System'),
+          onChanged: (val) => {setState(() => _current = val!)},
+        ),
+        RadioListTile<ThemeMode>(
+          value: ThemeMode.dark,
+          groupValue: _current,
+          title: const Text('Dark'),
+          onChanged: (val) => {setState(() => _current = val!)},
+        ),
+        RadioListTile<ThemeMode>(
+          value: ThemeMode.light,
+          groupValue: _current,
+          title: const Text('Light'),
+          onChanged: (val) => {setState(() => _current = val!)},
+        ),
+      ],
+    )));
+  }
+}
 
-        child: Column(
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key, required this.index}) : super(key: key);
+  final int index;
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          color: Colors.yellow.withOpacity(.5),
+          borderRadius: BorderRadius.circular(10),
+          image: const DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: NetworkImage(
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
+      title: const Text(
+        'Pikachu',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: const Text(
+        '⚡️electric',
+      ),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () => {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) => const PokeDetial(),
+        ))
+      },
     );
   }
 }
---
+++ lib/poke_detail.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class PokeDetial extends StatelessWidget {
+  const PokeDetial({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pokemon'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    'No.25',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Text(
+              'pikachu',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            Chip(
+              backgroundColor: Colors.yellow,
+              label: Text(
+                'electroc',
+                style: TextStyle(
+                    color: Colors.yellow.computeLuminance() > 0.5
+                        ? Colors.black
+                        : Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
